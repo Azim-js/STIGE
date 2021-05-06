0401721ecd4d647186d15d441bf52da1c0d84471
azim <az1234im@gmail.com>
2021-05-07 00:32:56 +0530

    final change

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const fs = require('fs');
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    // fs.readFile(__dirname, +'index.html');
+    res.writeHead(200, { 'content-type': 'text/html' });
+    fs.createReadStream('index.html').pipe(res);
+  });
+ 
+  app.listen(3000, () => {
+    console.log('Server Running.');
+  });
